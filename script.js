8e1c909e94c6a50089259d9d8280d3634a20c2e6
Teboho Nthako <tjaynthako@gmail.com>
2023-05-15 21:45:56 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let favoriteMovies = [
+  "Pursuit of Happyness",
+  "Acronomy",
+  "We have A Ghost",
+  "The Glass Onion",
+  "Bad Boys",
+];
+
+// Console logging the first and last movies in my favoriteMovies Array
+
+console.log(favoriteMovies[0]); // Outputs: Pursuit of Happyness
+console.log(favoriteMovies[4]); // Outputs: Bad Boys
+
+// Adding a new movie to my array with a number data type
+favoriteMovies[5] = 7;
+
+console.log(favoriteMovies[5]);
